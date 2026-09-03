fba1f0e7ed9062e1d7f59663340a74f9f0f978bb
Dillon <dmuldoon8801@sdsu.edu>
2026-09-03 14:32:02 -0700

    Add files via upload

---
+++ Fluent UDFs and Post Processing/Post Scripts/reorderFaces.m
@@ -0,0 +1,18 @@
+function fixedMatrix = reorderFaces(Tbl)
+headers = Tbl.Properties.VariableNames;
+headers = headers(2:end);
+fixedHeaders=zeros(size(headers));
+
+for i=1:numel(headers)
+    tempStore = char(headers(i));
+    tempStore = tempStore(2:end-4);
+    tempStore = strrep(tempStore,'_','.');
+    tempStore = str2double(tempStore);
+    fixedHeaders(i) = tempStore;
+end
+tempMatrix= table2array(Tbl(2:end,2:end));
+tempMatrix = [fixedHeaders; tempMatrix];
+[~,idx] = sort(tempMatrix(1,:),'ascend');
+tempMatrix = tempMatrix(:,idx);
+fixedMatrix = tempMatrix(2:end,:);
+end
