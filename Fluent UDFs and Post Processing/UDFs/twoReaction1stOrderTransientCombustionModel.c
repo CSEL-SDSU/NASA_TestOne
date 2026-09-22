446770e4289b1fb6f6546531f147762f1c1c456a
Dillon <dmuldoon8801@sdsu.edu>
2026-09-22 13:06:12 -0700

    Update twoReaction1stOrderTransientCombustionModel.c

--- Fluent UDFs and Post Processing/UDFs/twoReaction1stOrderTransientCombustionModel.c
+++ Fluent UDFs and Post Processing/UDFs/twoReaction1stOrderTransientCombustionModel.c
@@ -1,5 +1,5 @@
 /**********************************************************************
-  twoEquationTransientCombustionModel.c 
+  twoReaction1stOrderTransientCombustionModell.c 
   Header file containing Fluent User Defined Functions (UDFs) for a boundary condition pyrolysis model for transient combusiton: pyrolysis_release_rate, heat_absorption_rate, massSum
   !Caution massSum will overwrite the three csv files it outputs if they are not moved or renamed!
  ***********************************************************************/
